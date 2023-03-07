91c877d3d792d4e510c441827609795947e3f634
mrashz <127166750+mrashz@users.noreply.github.com>
2023-03-07 12:33:23 +0800

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -38,10 +38,10 @@ else:
     latest_iteration = st.empty()
     bar = st.progress(0)
 
-    for i in range(1000):
+    for i in range(100):
    
         latest_iteration.text(f'Iteration {i+1}')
-        bar.progress(i/10 + 1)
+        bar.progress(i + 1)
         time.sleep(0.1)
 
     '...and now we\'re done!'
